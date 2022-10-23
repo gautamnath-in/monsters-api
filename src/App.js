292004790ff91f2bc92fcb000643851fef5d55e0
gautam.nath.ext@boehringer-ingelheim.com <gautam.nath.ext@boehringer-ingelheim.com>
2022-10-23 17:42:54 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { CardList } from './components/card-lists/card-list.component';
+import { SearchBox } from './components/seacrh-box/search-box.component';
+
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      monsters: [],
+      searchField: ''
+    };
+
+    // this.handleChange = this.handleChange.bind(this);
+    // // bind:is a method on any function that returns a new function
+    // // where the context of this is set to whatever we passed to it.
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(users => this.setState({ monsters: users }))
+  }
+
+  handleChange = (e) => {
+    this.setState({ searchField: e.target.value })
+  }
+
+  render() {
+    // const monsters = this.state.monsters;
+    // const searchField = this.state.searchField;
+    // _____________OR_______________
+
+    const { monsters, searchField } = this.state;
+
+    const filteredMonsters = monsters.filter(monster => monster.name.toLowerCase().includes(searchField.toLowerCase()))
+
+
+
+    return (
+      <div className="App">
+        <h1 className='title'>MONSTERS ROLODEX</h1>
+        <SearchBox
+          placeholder='search monsters'
+          handleChange={this.handleChange} />
+        <CardList monsters={filteredMonsters} />
+      </div>
+    );
+  }
 }
+
 
 export default App;
---
+++ src/components/card/card.component.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import './card.styles.css'
+
+export const Card = ({ monster }) => (
+    <div className="card-container">
+        <img src={`https://robohash.org/${monster.id}?size=180x180`} />
+        <h1>{monster.name}</h1>
+        <p>{monster.email}</p>
+    </div>
+)
